71d6c05c9864eaff2d367fdd5afb996d8c23e247
BoyuHuo <boyu.huo.china@gmail.com>
2019-11-10 21:32:24 -0500

    init Main class, FPTree

---
+++ src/FPTree.java
@@ -0,0 +1,11 @@
+import java.util.Map;
+import java.util.Set;
+
+public class FPTree {
+    private long minSupport = 0;
+    private long basketNum = 0;
+
+    private static Map<Set<Node>, Long> fpGrowth(Node root, Map<String, Node> header, String idName) {
+        return null;
+    }
+}
