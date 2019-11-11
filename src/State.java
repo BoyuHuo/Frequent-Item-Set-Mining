563fdd87af30076d7e76fde6935857e339303c3a
BoyuHuo <boyu.huo.china@gmail.com>
2019-11-10 22:08:05 -0500

    add state recored class

---
+++ src/State.java
@@ -0,0 +1,11 @@
+import java.util.Map;
+
+class State {
+    public final Node root;
+    public final Map<String, Node> header;
+
+    public State(Node root, Map<String, Node> header) {
+        this.root = root;
+        this.header = header;
+    }
+}
