947307b86067ceb99c5d7efc12f3af4a1e260c7f
BoyuHuo <boyu.huo.china@gmail.com>
2019-11-10 22:24:45 -0500

    add Header calculate and order line calculate

--- src/FPTree.java
+++ src/FPTree.java
@@ -5,16 +5,16 @@ public class FPTree {
     private long basketNum = 0;
 
 
-    private static Map<Set<Node>, Long> fpGrowth(Node root, Map<String, Node> header, String idName) {
+    private Map<Set<Node>, Long> fpGrowth(Node root, Map<String, Node> header, String idName) {
         Map<Set<Node>, Long> conditionFres = new HashMap<Set<Node>, Long>();
         Set<String> keys = header.keySet();
         String[] keysArray = keys.toArray(new String[0]);
         String firstIdName = keysArray[keysArray.length - 1];
-        if (isSinglePath(header, firstIdName)) {// ֻ��һ��·��ʱ����·���ϵ�������ϼ��ɵõ�����Ƶ����
+        if (isSinglePath(header, firstIdName)) {
             if (idName == null)
                 return conditionFres;
             Node leaf = header.get(firstIdName);
-            List<Node> paths = new ArrayList<Node>();// �Զ����ϱ���·�����
+            List<Node> paths = new ArrayList<Node>();
             paths.add(leaf);
             Node node = leaf;
             while (node.parent.idName != null) {
@@ -26,7 +26,7 @@ public class FPTree {
             conditionFres = addLeafToFrequent(tempNode, conditionFres);
 
         } else {
-            for (int i = keysArray.length - 1; i >= 0; i--) {// �ݹ�����������Ƶ����
+            for (int i = keysArray.length - 1; i >= 0; i--) {
                 String key = keysArray[i];
                 List<Node> leafs = new ArrayList<Node>();
                 Node link = header.get(key);
@@ -60,8 +60,6 @@ public class FPTree {
                 }
                 State holder = getConditionFpTree(paths);
                 if (holder.header.size() != 0) {
-                    // if (idName != null)
-                    // key = idName + " " + key;
                     Map<Set<Node>, Long> preFres = fpGrowth(holder.root, holder.header, key);
                     if (idName != null) {
                         Node tempNode = new Node(idName, leafCount);
@@ -82,14 +80,14 @@ public class FPTree {
         return false;
     }
 
-    private static State getConditionFpTree(Map<List<String>, Long> paths) {
+    private State getConditionFpTree(Map<List<String>, Long> paths) {
         List<String[]> matrix = new ArrayList<String[]>();
         for (Map.Entry<List<String>, Long> entry : paths.entrySet()) {
             for (long i = 0; i < entry.getValue(); i++) {
                 matrix.add(entry.getKey().toArray(new String[0]));
             }
         }
-        Map<String, Integer> frequentMap = new LinkedHashMap<String, Integer>();// һ��Ƶ����
+        Map<String, Integer> frequentMap = new LinkedHashMap<String, Integer>();
         Map<String, Node> cHeader = getHeader(matrix, frequentMap);
         Node cRoot = getFpTree(matrix, cHeader, frequentMap);
         return new State(cRoot, cHeader);
@@ -99,9 +97,8 @@ public class FPTree {
             List<Node> paths, String idName) {
         Map<Set<Node>, Long> conditionFres = new HashMap<Set<Node>, Long>();
         int size = paths.size();
-        for (int mask = 1; mask < (1 << size); mask++) {// ��������ϣ���1��ʼ��ʾ���Կռ�
+        for (int mask = 1; mask < (1 << size); mask++) {
             Set<Node> set = new HashSet<Node>();
-            // �ҳ�ÿ�ο��ܵ�ѡ��
             for (int i = 0; i < paths.size(); i++) {
                 if ((mask & (1 << i)) > 0) {
                     set.add(paths.get(i));
@@ -123,9 +120,6 @@ public class FPTree {
         int count = 0;
         for (String[] line : matrix) {
             String[] orderLine = getOrderLine(line, frequentMap);
-//			count++;
-//			if (count % 100000 == 0)
-//				System.out.println(count);
             Node parent = root;
             for (String idName : orderLine) {
                 int index = parent.hasChild(idName);
@@ -153,13 +147,61 @@ public class FPTree {
     }
 
     private static String[] getOrderLine(String[] line, Map<String, Integer> frequentMap) {
-
-        return null;
+        Map<String, Integer> countMap = new HashMap<String, Integer>();
+        for (String id : line) {
+            if (frequentMap.containsKey(id)) {
+                countMap.put(id, frequentMap.get(id));
+            }
+        }
+        List<Map.Entry<String, Integer>> mapList = new ArrayList<Map.Entry<String, Integer>>(
+                countMap.entrySet());
+        Collections.sort(mapList, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> v1,
+                               Map.Entry<String, Integer> v2) {
+                return v2.getValue() - v1.getValue();
+            }
+        });
+        String[] orderLine = new String[countMap.size()];
+        int i = 0;
+        for (Map.Entry<String, Integer> entry : mapList) {
+            orderLine[i] = entry.getKey();
+            i++;
+        }
+        return orderLine;
     }
 
-    private static Map<String, Node> getHeader(List<String[]> matrix, Map<String, Integer> frequentMap) {
-
-        return null;
+    private Map<String, Node> getHeader(List<String[]> matrix, Map<String, Integer> frequentMap) {
+        Map<String, Integer> countMap = new HashMap<String, Integer>();
+        for (String[] line : matrix) {
+            for (String idName : line) {
+                if (countMap.containsKey(idName)) {
+                    countMap.put(idName, countMap.get(idName) + 1);
+                } else {
+                    countMap.put(idName, 1);
+                }
+            }
+        }
+        for (Map.Entry<String, Integer> entry : countMap.entrySet()) {
+            if (entry.getValue() >= this.minSupport)
+                frequentMap.put(entry.getKey(), entry.getValue());
+        }
+        List<Map.Entry<String, Integer>> mapList = new ArrayList<Map.Entry<String, Integer>>(
+                frequentMap.entrySet());
+        Collections.sort(mapList, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> v1,
+                               Map.Entry<String, Integer> v2) {
+                return v2.getValue() - v1.getValue();
+            }
+        });
+        frequentMap.clear();
+        Map<String, Node> header = new LinkedHashMap<String, Node>();
+        for (Map.Entry<String, Integer> entry : mapList) {
+            header.put(entry.getKey(), null);
+            frequentMap.put(entry.getKey(), entry.getValue());
+        }
+        return header;
     }
 
     private static Map<Set<Node>, Long> addLeafToFrequent(Node leaf, Map<Set<Node>, Long> conditionFres) {
